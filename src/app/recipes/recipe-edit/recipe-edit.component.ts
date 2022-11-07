d742eeff7b423bf35b1c0ae5850b2c9aaa478018
мак <mak@MacBook-Pro-mak.local>
2022-11-07 14:21:02 +0200

    d

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipesService } from 'src/app/shared/services/recipesService/recipes.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -8,18 +9,64 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./recipe-edit.component.scss']
 })
 export class RecipeEditComponent implements OnInit {
-id: number | undefined;
+id: any;
 editMode = false;
-editResipeForm: FormGroup;
+editResipeForm: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute, 
+    private fb: FormBuilder,
+    private recipeService: RecipesService,
+    ) {
+    this.editResipeForm = fb.group({
+      name: fb.control('', Validators.required),
+      imagePath: fb.control('', Validators.required),
+      description: fb.control('', Validators.required),
+      number: fb.control('', Validators.required),
+    })
+  }
 
   ngOnInit(): void {
     this.route.params
     .subscribe((params: Params) => {
-      this.id = +params['id'];
+      this.id = +params['id'] - 1;
       this.editMode = params['id'] != null;
+      this.setOurForm();
+    })
+
+  }
+
+  setOurForm() {
+    const recipe = this.recipeService.getRecipe(this.id)
+
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
+    if(this.editMode) {
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+      if(recipe['ingredients']) {
+        for(let ingredient of recipe.ingredients) {
+          recipeIngredients.push(new FormGroup({
+            name: this.fb.control(ingredient.name),
+            amount: this.fb.control(ingredient.amount),
+          }));
+        }
+      }
+    }
+
+    this.editResipeForm.setValue({
+      name: recipeName,
+      imagePath: recipeImagePath,
+      description: recipeDescription,
+      ingredients: recipeIngredients,
+      number: ''
     })
   }
 
+  get controls() {
+    return (<FormArray>this.editResipeForm.get('ingredients'))?.controls;
+  }
 }
