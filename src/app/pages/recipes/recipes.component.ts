c4c7e67b16e18c068f9faba779560294f640eb86
мак <mak@MacBook-Pro-mak.local>
2022-11-09 10:34:48 +0200

    d

--- src/app/pages/recipes/recipes.component.ts
+++ src/app/pages/recipes/recipes.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { PlaceholderDirective } from 'src/app/shared/directives/placeholder/placeholder.directive';
 import { CustomeValidators } from '../../shared/custome-validators/custome-validators';
+import { SuccessAlertComponent } from '../success-alert/success-alert.component';
 
 @Component({
   selector: 'app-recipes',
@@ -8,8 +10,12 @@ import { CustomeValidators } from '../../shared/custome-validators/custome-valid
   styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
+  @ViewChild(PlaceholderDirective)alertHost? : PlaceholderDirective;
   myForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private componentFactoryResolve: ComponentFactoryResolver
+    ) {
     this.myForm = this.fb.group({
       name: this.fb.control('hello', [Validators.required, CustomeValidators.invalidInput])
     })
@@ -17,6 +23,18 @@ export class RecipesComponent implements OnInit {
 
   ngOnInit(): void {
     
+  }
+
+  onShowSuccessMessage() {
+    const alertSuccess = this.componentFactoryResolve.resolveComponentFactory(
+        SuccessAlertComponent
+      );
+
+      const hostViewContainerRef = this.alertHost?.viewContainer;
+
+      const componentRef: any = hostViewContainerRef?.createComponent(alertSuccess);
+
+      componentRef.instance.message = 'BBOY LOX'
   }
 
 }
